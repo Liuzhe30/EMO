f8b848841ca3e965aa0b5ee15491c3afd78f8493
Zhe Liu <liuzlm1030@sjtu.edu.cn>
2023-05-08 22:53:47 +0800

    clean sequence

--- preprocess/scripts/8_dataset_split.py
+++ preprocess/scripts/8_dataset_split.py
@@ -4,6 +4,10 @@ from pandas import read_parquet
 pd.set_option('display.max_columns', None)
 #pd.set_option('display.max_rows', None)
 
+gene_dict = {'A':[0,0,0,1], 'T':[0,0,1,0], 'C':[0,1,0,0], 'G':[1,0,0,0], 
+             'a':[0,0,0,1], 't':[0,0,1,0], 'c':[0,1,0,0], 'g':[1,0,0,0],
+             } 
+
 # dataset path
 data_path_enhancer = '/data/eqtl/datasets/enhancer/'
 data_path_repressor = '/data/eqtl/datasets/repressor/'
@@ -28,6 +32,22 @@ for bulk in gtex_bulk_list:
 
     merged_df = pd.concat([data_en, data_re])
     merged_df = merged_df.drop(['atac_tss_51', 'tss_51_seq','level_0','index'], axis=1)
+
+    # delete wrong sequence
+    del_row = []
+    for i in range(len(merged_df)):
+        for str in merged_df['variant_51_seq'].values[i]:
+            if(str not in gene_dict.keys() and merged_df['variant_id'].values[i] not in del_row):
+                del_row.append(merged_df['variant_id'].values[i])
+        for str in merged_df['variant_51_seq_after_mutation'].values[i]:
+            if(str not in gene_dict.keys() and merged_df['variant_id'].values[i] not in del_row):
+                del_row.append(merged_df['variant_id'].values[i])   
+        for str in merged_df['seq_between_variant_tss'].values[i]:
+            if(str not in gene_dict.keys() and merged_df['variant_id'].values[i] not in del_row):
+                del_row.append(merged_df['variant_id'].values[i])  
+    #print(del_row)
+    for i in del_row:
+        merged_df = merged_df[~(merged_df['variant_id'] == i)]
 
     shuffle_df = merged_df.sample(frac=1).reset_index(drop=True)
 
--- preprocess/scripts/9_generate_datasets.py
+++ preprocess/scripts/9_generate_datasets.py
@@ -11,14 +11,14 @@ test_df = pd.DataFrame()
 
 for i in range(len(train_all)):        
     tss_distance = abs(int(train_all['tss_distance'].values[i]))
-    if(tss_distance <= 1000):
-        train_df = pd.concat([train_df, train_all[i:i+1]])
+    if(tss_distance < 1000):
+        train_df = pd.concat([train_df, train_all.iloc[i]])
 train_df = train_df.reset_index(drop=True)
 
 for i in range(len(test_all)):        
     tss_distance = abs(int(test_all['tss_distance'].values[i]))
-    if(tss_distance <= 1000):
-        test_df = pd.concat([test_df, test_all[i:i+1]])
+    if(tss_distance < 1000):
+        test_df = pd.concat([test_df, test_all.iloc[i]])
 test_df = test_df.reset_index(drop=True)
 
 train_df.to_pickle('../../datasets/small/train_small.pkl')
@@ -30,14 +30,14 @@ test_df = pd.DataFrame()
 
 for i in range(len(train_all)):        
     tss_distance = abs(int(train_all['tss_distance'].values[i]))
-    if(tss_distance > 1000 and tss_distance <= 10000):
-        train_df = pd.concat([train_df, train_all[i:i+1]])
+    if(tss_distance >= 1000 and tss_distance < 10000):
+        train_df = pd.concat([train_df, train_all.iloc[i]])
 train_df = train_df.reset_index(drop=True)
 
 for i in range(len(test_all)):        
     tss_distance = abs(int(test_all['tss_distance'].values[i]))
-    if(tss_distance > 1000 and tss_distance <= 10000):
-        test_df = pd.concat([test_df, test_all[i:i+1]])
+    if(tss_distance >= 1000 and tss_distance < 10000):
+        test_df = pd.concat([test_df, test_all.iloc[i]])
 test_df = test_df.reset_index(drop=True)
 
 train_df.to_pickle('../../datasets/middle/train_middle.pkl')
@@ -49,14 +49,14 @@ test_df = pd.DataFrame()
 
 for i in range(len(train_all)):        
     tss_distance = abs(int(train_all['tss_distance'].values[i]))
-    if(tss_distance > 10000 and tss_distance <= 100000):
-        train_df = pd.concat([train_df, train_all[i:i+1]])
+    if(tss_distance >= 10000 and tss_distance < 100000):
+        train_df = pd.concat([train_df, train_all.iloc[i]])
 train_df = train_df.reset_index(drop=True)
 
 for i in range(len(test_all)):        
     tss_distance = abs(int(test_all['tss_distance'].values[i]))
-    if(tss_distance > 10000 and tss_distance <= 100000):
-        test_df = pd.concat([test_df, test_all[i:i+1]])
+    if(tss_distance >= 10000 and tss_distance < 100000):
+        test_df = pd.concat([test_df, test_all.iloc[i]])
 test_df = test_df.reset_index(drop=True)
 
 train_df.to_pickle('../../datasets/large/train_large.pkl')
@@ -68,14 +68,14 @@ test_df = pd.DataFrame()
 
 for i in range(len(train_all)):        
     tss_distance = abs(int(train_all['tss_distance'].values[i]))
-    if(tss_distance > 100000):
-        train_df = pd.concat([train_df, train_all[i:i+1]])
+    if(tss_distance >= 100000):
+        train_df = pd.concat([train_df, train_all.iloc[i]])
 train_df = train_df.reset_index(drop=True)
 
 for i in range(len(test_all)):        
     tss_distance = abs(int(test_all['tss_distance'].values[i]))
-    if(tss_distance > 100000):
-        test_df = pd.concat([test_df, test_all[i:i+1]])
+    if(tss_distance >= 100000):
+        test_df = pd.concat([test_df, test_all.iloc[i]])
 test_df = test_df.reset_index(drop=True)
 
 train_df.to_pickle('../../datasets/huge/train_huge.pkl')

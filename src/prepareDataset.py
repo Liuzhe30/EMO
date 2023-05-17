b46be63d7abe699bb971b598f9da68c71b230d20
Zhe Liu <liuzlm1030@sjtu.edu.cn>
2023-05-17 17:33:08 +0800

    update data augmentation and reverse testset

--- src/prepareDataset.py
+++ src/prepareDataset.py
@@ -1,6 +1,7 @@
-# prepare .npy datasets with shuffle (easy to OOM, Data Generator is suggested)
+# shuffle & data augmentation
 import pandas as pd
 import numpy as np
+from sklearn.utils import shuffle
 pd.set_option('display.max_columns', None)
 
 gene_dict = {'A':[0,0,0,1], 'T':[0,0,1,0], 'C':[0,1,0,0], 'G':[1,0,0,0], 
@@ -12,7 +13,111 @@ label_dict = {'0':0, # Enhancer
               '1':1, # Repressor
               }
 
-def pkl2npy(model_size):
+def augmentation(model_size): # try data augmentation for train set
+    path = '../datasets/' + model_size + '/train_' + model_size + '.pkl'
+    out = '../datasets/' + model_size + '/train_aug_' + model_size + '.pkl'
+
+    train_set = pd.read_pickle(path)
+    #print(train_set)
+    column_list = ['variant_51_seq', 'seq_between_variant_tss', 'variant_51_seq_after_mutation', 'atac_between', 'atac_variant_51', 'label']
+    data_new_train = {'variant_51_seq':[],  'seq_between_variant_tss':[], 'variant_51_seq_after_mutation':[], 
+                  'atac_between':[], 'atac_variant_51':[],  'label':[], 'slope':[]}
+
+    for i in range(len(train_set)):
+        variant_51_seq = train_set['variant_51_seq'].values[i]
+        seq_between_variant_tss = train_set['seq_between_variant_tss'].values[i]    
+        variant_51_seq_after_mutation = train_set['variant_51_seq_after_mutation'].values[i]
+        seq_between_variant_tss_after_mutation = train_set['seq_between_variant_tss_after_mutation'].values[i]
+        atac_between = train_set['atac_between'].values[i]
+        atac_variant_51 = train_set['atac_variant_51'].values[i]
+        label = train_set['label'].values[i]
+        slope = float(train_set['slope'].values[i])
+    
+        # original 
+        data_new_train['variant_51_seq'].append(variant_51_seq)
+        data_new_train['seq_between_variant_tss'].append(seq_between_variant_tss)
+        data_new_train['variant_51_seq_after_mutation'].append(variant_51_seq_after_mutation)
+        data_new_train['atac_between'].append(atac_between)
+        data_new_train['atac_variant_51'].append(atac_variant_51)
+        data_new_train['label'].append(label)
+        data_new_train['slope'].append(slope)
+        
+        # reverse
+        if(str(label) == '0'):        
+            data_new_train['variant_51_seq'].append(variant_51_seq_after_mutation)
+            data_new_train['seq_between_variant_tss'].append(seq_between_variant_tss_after_mutation)
+            data_new_train['variant_51_seq_after_mutation'].append(variant_51_seq)
+            data_new_train['atac_between'].append(atac_between)
+            data_new_train['atac_variant_51'].append(atac_variant_51)
+            data_new_train['slope'].append(slope*(-1))
+            data_new_train['label'].append('1')  
+        elif(str(label) == '1'):
+            data_new_train['variant_51_seq'].append(variant_51_seq_after_mutation)
+            data_new_train['seq_between_variant_tss'].append(seq_between_variant_tss_after_mutation)
+            data_new_train['variant_51_seq_after_mutation'].append(variant_51_seq)
+            data_new_train['atac_between'].append(atac_between)
+            data_new_train['atac_variant_51'].append(atac_variant_51)
+            data_new_train['slope'].append(slope*(-1))
+            data_new_train['label'].append('0')
+        
+    train_new = pd.DataFrame(data_new_train)
+    train_new = shuffle(train_new)
+
+    c_re, c_en = 0, 0
+    for i in range(len(train_new)):
+        label = train_new['label'].values[i]
+        if(str(label) == '0'):
+            c_re += 1
+        elif(str(label) == '1'):
+            c_en += 1  
+    print(c_re)
+    print(c_en)
+
+    train_new.to_pickle(out)
+
+def reverse_testset(model_size):
+    path = '../datasets/' + model_size + '/test_' + model_size + '.pkl'
+    out = '../datasets/' + model_size + '/test_rev_' + model_size + '.pkl'
+
+    test_set = pd.read_pickle(path)
+    #print(test_set)
+    column_list = ['variant_51_seq', 'seq_between_variant_tss', 'variant_51_seq_after_mutation', 'atac_between', 'atac_variant_51', 'label']
+    data_new_test = {'variant_51_seq':[],  'seq_between_variant_tss':[], 'variant_51_seq_after_mutation':[], 
+                  'atac_between':[], 'atac_variant_51':[],  'label':[], 'slope':[]}
+
+    for i in range(len(test_set)):
+        variant_51_seq = test_set['variant_51_seq'].values[i]
+        seq_between_variant_tss = test_set['seq_between_variant_tss'].values[i]    
+        variant_51_seq_after_mutation = test_set['variant_51_seq_after_mutation'].values[i]
+        seq_between_variant_tss_after_mutation = test_set['seq_between_variant_tss_after_mutation'].values[i]
+        atac_between = test_set['atac_between'].values[i]
+        atac_variant_51 = test_set['atac_variant_51'].values[i]
+        label = test_set['label'].values[i]
+        slope = float(test_set['slope'].values[i])
+        
+        # reverse
+        if(str(label) == '0'):        
+            data_new_test['variant_51_seq'].append(variant_51_seq_after_mutation)
+            data_new_test['seq_between_variant_tss'].append(seq_between_variant_tss_after_mutation)
+            data_new_test['variant_51_seq_after_mutation'].append(variant_51_seq)
+            data_new_test['atac_between'].append(atac_between)
+            data_new_test['atac_variant_51'].append(atac_variant_51)
+            data_new_test['slope'].append(slope*(-1))
+            data_new_test['label'].append('1')  
+        elif(str(label) == '1'):
+            data_new_test['variant_51_seq'].append(variant_51_seq_after_mutation)
+            data_new_test['seq_between_variant_tss'].append(seq_between_variant_tss_after_mutation)
+            data_new_test['variant_51_seq_after_mutation'].append(variant_51_seq)
+            data_new_test['atac_between'].append(atac_between)
+            data_new_test['atac_variant_51'].append(atac_variant_51)
+            data_new_test['slope'].append(slope*(-1))
+            data_new_test['label'].append('0')
+        
+    test_new = pd.DataFrame(data_new_test)
+    test_new = shuffle(test_new)
+    test_new.to_pickle(out)
+
+def pkl2npy(model_size): # prepare .npy datasets with shuffle (easy to OOM, Data Generator is suggested)
     
     trainpath = '../datasets/' + model_size + '/train_' + model_size + '.pkl'
     testpath = '../datasets/' + model_size + '/test_' + model_size + '.pkl'
@@ -105,12 +210,21 @@ def pkl2npy(model_size):
             np.save('../datasets/' + model_size + '/ori/test_atac_bet.npy', np_atac_bet)
             np.save('../datasets/' + model_size + '/ori/test_label.npy', np_label)
 
-def augmentation():
-    pass
-
 if __name__ == '__main__':
     
+    augmentation('small')
+    augmentation('middle')
+    augmentation('large')
+    augmentation('huge')
+
+    reverse_testset('small')
+    reverse_testset('middle')
+    reverse_testset('large')
+    reverse_testset('huge')
+
+    '''
     pkl2npy('small')   
     pkl2npy('middle')
     #pkl2npy('large') # easy to OOM
-    #pkl2npy('huge')  # easy to OOM                                                                                                               
+    #pkl2npy('huge')  # easy to OOM    
+    '''                                                                                                           

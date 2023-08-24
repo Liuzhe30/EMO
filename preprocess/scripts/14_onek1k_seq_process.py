2687ae93f4e264cb9ec6d596ec3b2412f1c2e607
Zhe Liu <liuzlm1030@sjtu.edu.cn>
2023-08-24 20:29:30 +0800

    mapping mutation to hg38 sequence

---
+++ preprocess/scripts/14_onek1k_seq_process.py
@@ -0,0 +1,61 @@
+import os
+import numpy as np
+import pandas as pd
+
+# load hg38 fasta
+fasta_path = '../../datasets/chr_fasta_hg38/'
+
+
+cell_list = ['B_Naive','CD4_memory','CD4_naive','CD8_memory','Dendritic_Cell','Natural_killer_Cell']
+for cell in cell_list:
+    data = pd.read_pickle('../../datasets/sceQTL/merged/' + cell + '_hg38.pkl')
+
+    data['variant_51_seq'] = 0
+    data['tss_51_seq'] = 0
+    data['seq_between_variant_tss'] = 0
+    data['variant_51_seq_after_mutation'] = 0
+    data['seq_between_variant_tss_after_mutation'] = 0
+
+    for i in range(len(data)):
+        chr_str = 'chr' + str(data['CHR'][i])
+        position = data['POS'][i]
+        tss_position = data['TSS'][i]
+        before_mutation = data['A1'][i]
+        after_mutation = data['A2'][i]
+        tss_distance = position - tss_position
+
+        with open(fasta_path + chr_str + '_new.fasta') as fa:
+            line = fa.readline()
+            if(line[position - 1].upper() != before_mutation):
+                print('error!')
+
+            variant_51_seq = line[position - 26:position + 25]  
+            #print(variant_51_seq)
+            tss_51_seq = line[tss_position - 26:tss_position + 25]
+            #print(tss_51_seq)
+            if(tss_distance > 0):
+                seq_between_variant_tss = line[tss_position - 1:position]
+            else:
+                seq_between_variant_tss = line[position -1: tss_position]
+            #print(seq_between_variant_tss)
+            if(line[position - 1] >= 'a' and line[position - 1] <= 'z'):
+                variant_51_seq_after_mutation = line[position - 26: position - 1] + after_mutation.lower() + line[position: position + 25]
+                if(tss_distance > 0):
+                    seq_between_variant_tss_after_mutation = seq_between_variant_tss[0:-2] + after_mutation.lower()
+                else:
+                    seq_between_variant_tss_after_mutation = after_mutation.lower() + seq_between_variant_tss[1:-1]
+            else:
+                variant_51_seq_after_mutation = line[position - 26: position - 1] + after_mutation + line[position: position + 25]
+                if(tss_distance > 0):
+                    seq_between_variant_tss_after_mutation = seq_between_variant_tss[0:-2] + after_mutation
+                else:
+                    seq_between_variant_tss_after_mutation = after_mutation + seq_between_variant_tss[1:-1]                    
+            #print(variant_51_seq_after_mutation)
+                
+            data.loc[i, 'variant_51_seq'] = variant_51_seq
+            data.loc[i, 'tss_51_seq'] = tss_51_seq
+            data.loc[i, 'seq_between_variant_tss'] = seq_between_variant_tss
+            data.loc[i, 'variant_51_seq_after_mutation'] = variant_51_seq_after_mutation
+            data.loc[i, 'seq_between_variant_tss_after_mutation'] = seq_between_variant_tss_after_mutation
+    print(data)
+    data.to_pickle('../../datasets/sceQTL/merged/' + cell + '_seq.pkl')

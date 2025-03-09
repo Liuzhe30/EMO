98a726ddfcd5110d47bc4e84293b8503d8f57841
Zhe Liu <liuzlm1030@sjtu.edu.cn>
2025-03-09 19:18:54 +0800

    psymukb SNP prediction

---
+++ experiment/13-psymukb-analysis/03-running-interface.py
@@ -0,0 +1,38 @@
+import pandas as pd
+import math
+import numpy as np
+pd.set_option('display.max_columns', None)
+
+from src.utils_sign_prediction import *
+from src.utils_slope_prediction import *
+
+window_len = 51
+disease_list = ['Autism_ASD','Developmental_Delay_DD','Fetal_non-Preterm_birth_non-PTB','Fetal_preterm_birth_PTB','Mix_Autism_or_Schizophrenia','Sibling_Control']
+# 1000,24,863,497,198,1000
+tissue_list = ['hippocampus','cerebellum','prefrontal_cortex','temporal_lobe','substantia_nigra']
+
+# Define path of reference genome 
+genome_path = 'reference_genome_hg38/' # In this case, 'reference_genome_hg38/chr19.fasta' will be used.
+# Define path of pretrained model weights 
+weights_path = 'trained_weights/' #  In this case, 'trained_weights/small_trained_weights.tf' and 'trained_weights/small_slope_weights.tf' will be used.
+
+for disease in disease_list:
+    data = pd.read_pickle('datasets/' + disease + '_post.pkl')
+    for tissue in tissue_list:
+        data[tissue + '_slope'] = 0
+        for i in range(len(data)):
+            chr_no = data['Chr'].values[i]
+            ref = data['Ref'].values[i]
+            alt = data['Alt'].values[i]
+            position = int(data['position'].values[i])
+
+            input_variant = 'chr' + str(chr_no) + '_' + str(position) + '_' + ref + '_' + alt
+            TSS_distance = int(data['TSS_distance'].values[i])
+            atac_between = np.array(data[tissue + '_between'].values[i])
+            atac_variant = np.array(data[tissue + '_variant'].values[i])
+
+            slope_prediction_output = get_slope_prediction_result(input_variant, TSS_distance, atac_variant, atac_between, genome_path, weights_path) 
+            data[tissue + '_slope'][i] = slope_prediction_output
+        print(data.head())    
+    print(data.head()) 
+    data.to_pickle('datasets/' + disease + '_pred.pkl')
---
+++ experiment/13-psymukb-analysis/04-fetch-prediction-results.py
@@ -0,0 +1,20 @@
+import pandas as pd
+pd.set_option('display.max_columns', None)
+
+disease_list = ['Autism_ASD','Developmental_Delay_DD','Fetal_non-Preterm_birth_non-PTB','Fetal_preterm_birth_PTB','Mix_Autism_or_Schizophrenia','Sibling_Control']
+# 1000,24,863,497,198,1000
+tissue_list = ['hippocampus','cerebellum','prefrontal_cortex','temporal_lobe','substantia_nigra']
+
+for disease in disease_list:
+    data = pd.read_pickle('datasets/' + disease + '_pred.pkl')[['Chr','Ref','Alt','position'] + ['Chr','Ref','Alt','position'] + ['hippocampus_slope','cerebellum_slope','prefrontal_cortex_slope','temporal_lobe_slope','substantia_nigra_slope']]
+    data.to_csv('datasets/' + disease + '_pred_final.csv',index=False)
+
+'''
+Chr	Ref	Alt	position	hippocampus_slope	cerebellum_slope	prefrontal_cortex_slope	temporal_lobe_slope	substantia_nigra_slope
+9	T	C	92819845	0.09405224	0.040140826	0.109988414	0.099657565	0.067360893
+10	T	A	78996445	0.129093304	0.151506886	0.163972959	0.084876031	0.127745315
+10	A	G	120824851	0.186338976	0.14761126	0.251977026	0.338231802	0.232195571
+9	A	G	2614615	0.87506336	0.954618394	0.900415778	0.858251691	1.099137068
+14	G	T	97970375	0.473214	0.479877979	0.484405607	0.482635468	0.486697853
+
+'''
